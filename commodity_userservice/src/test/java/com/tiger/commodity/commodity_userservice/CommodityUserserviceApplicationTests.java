7119b9aa00a2708434ce2c612f23118c34cb4ba5
mytiger <2893171563@qq.com>
2020-01-06 14:39:09 +0800

    增加全局异常

---
+++ commodity_userservice/src/test/java/com/tiger/commodity/commodity_userservice/CommodityUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiger.commodity.commodity_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommodityUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
