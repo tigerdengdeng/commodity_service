69e7df78367f213381f699840dec40653f225e8d
mytiger <2893171563@qq.com>
2020-01-17 19:07:26 +0800

    初步增加网关 gateway 动态路由

---
+++ commodity_zuul_gateway/src/main/java/com/tiger/zuul/commodity_zuul_gateway/routedefinition/GatewayPredicateDefinition.java
@@ -0,0 +1,22 @@
+package com.tiger.zuul.commodity_zuul_gateway.routedefinition;
+
+
+import lombok.Data;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 动态路由 路由断言模型
+ *
+ * @author mytiger
+ * @date 2020-01-19
+ */
+@Data
+public class GatewayPredicateDefinition {
+
+    //断言对应的Name
+    private String name;
+    //配置的断言规则
+    private Map<String, String> args = new LinkedHashMap<>();
+}
